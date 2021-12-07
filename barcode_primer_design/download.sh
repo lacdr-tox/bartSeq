4e784272e8a6bd80bd271d626541107db1409a5a
Gerhard Burger <burger.ga@gmail.com>
2021-12-07 21:23:36 +0100

    Upload download instructions for databases

--- barcode_primer_design/download.sh
+++ barcode_primer_design/download.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-rsync -aP heidi:/home/icb/steffen.sass/data/{hg19ucsc,{hg38,mrna,refMrna}.fa}.{nhr,nin,nsq} ./databases/ 
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/refMrna.fa.gz
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/mrna.fa.gz
+
+gunzip -c hg38.fa.gz | makeblastdb -in - -dbtype nucl -out hg38.fa -title "UCSC Genome hg38"
+gunzip -c hg19.fa.gz | makeblastdb -in - -dbtype nucl -out hg19.fa -title "UCSC Genome hg19"
+gunzip -c mrna.fa.gz | makeblastdb -in - -dbtype nucl -out mrna.fa -title "GenBank mRNA"
+gunzip -c refMrna.fa.gz | makeblastdb -in - -dbtype nucl -out refMrna.fa -title "RefSeq mRNA"
